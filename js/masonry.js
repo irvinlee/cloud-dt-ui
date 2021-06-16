a41213b3188adbf89f5f2321b9b99758cbdb09ef
Irvin Lee Eduliantes <irvin.e@digitalroominc.com>
2021-06-17 00:22:45 +0800

    ie fixes

--- js/masonry.js
+++ js/masonry.js
@@ -1,11 +1,14 @@
+
 function resizeGridItems(gridClassName){
-    document.querySelectorAll(gridClassName).forEach(function(grid) {
-        grid.querySelectorAll(':scope > li').forEach(function(item) {
-            var rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-            var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-            var rowSpan = Math.floor((item.querySelector('img').getBoundingClientRect().height + rowGap)/(rowHeight + rowGap));
-            item.style.gridRowEnd = "span "+rowSpan;
-        });
+    var grid = document.querySelector(gridClassName);
+    var items = document.querySelectorAll(gridClassName + ' > li');
+    var itemsArray = Array.prototype.slice.call(items);
+
+    itemsArray.forEach(function(item) {
+        var rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+        var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+        var rowSpan = Math.floor((item.querySelector('img').getBoundingClientRect().height + rowGap)/(rowHeight + rowGap));
+        item.style.gridRowEnd = "span "+rowSpan;
     });
  }
 
