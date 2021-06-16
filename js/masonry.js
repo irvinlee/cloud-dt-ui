bf455842fb65103cb8447d5b2f129e98465f8ee8
Irvin Lee Eduliantes <irvin.e@digitalroominc.com>
2021-06-16 11:03:43 +0800

    fixed columns masonry

--- js/masonry.js
+++ js/masonry.js
@@ -3,7 +3,7 @@ function resizeGridItems(gridClassName){
         grid.querySelectorAll(':scope > li').forEach(function(item) {
             var rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
             var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-            var rowSpan = Math.ceil((item.querySelector('img').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+            var rowSpan = Math.floor((item.querySelector('img').getBoundingClientRect().height + rowGap)/(rowHeight + rowGap));
             item.style.gridRowEnd = "span "+rowSpan;
         });
     });
@@ -11,4 +11,4 @@ function resizeGridItems(gridClassName){
 
 
 
-document.addEventListener('DOMContentLoaded', resizeGridItems('.comp-dynamic-photo-grid'), false);
+document.addEventListener('DOMContentLoaded', resizeGridItems('.comp-masonry-grid.fixed-columns'), false);
